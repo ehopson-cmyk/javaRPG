407583e762790c040190950da0be7034ddd2e212
ehopson-cmyk <ehopson@greenwoodjax.org>
2025-08-28 12:54:57 -0400

    Update main.java

--- main.java
+++ main.java
@@ -50,42 +50,6 @@ public class main
 return coordinates; 	
       }
       
-      //fight time
-      
-      static int[] enemyFight /* output an int array for the fight rewards */ (int enemyType, int[] playerInfo, int currentLevel) {
-          //enemy setup
-          String[][] weakEnemyList = {{"Test Enemy", "t", "10", "2", /* stats: HP, STR */ "Dun dun dun! A test enemy approaches!", "The test enemy is chilling.", "Text", "50", "0", "The test enemy is preparing something...", "And nothing happened!" /* Special ability stuff: Whether they have one/type, what type of effect (in this case nothing happens), a chance to occur, & prepare + execute texts.*/}};
-           String[][] strongEnemyList = {{"Test STRONG Enemy", "T", "20", "3", /* stats: HP, STR */ "Dun dun dun! A strong test enemy approaches!", "The test enemy is looking like they're ready to rumble.", "Attack", "40", "6", "The test enemy is preparing something substanal...", "They detonate a nuclear warhead! Dealing 6 damage!" /* Special ability stuff: Whether they have one/type, what type of effect (in this case an attack), a chance to occur, & prepare + execute texts.*/}};
-          //pick enemy type (make better later) 
-          int enemyNumber = 0;
-          //setup enemy stats
-          int enemyHealth;
-          String enemyIcon;
-          
-          if (enemyType == 0) {
-                enemyHealth = Integer.parseInt(weakEnemyList[enemyNumber][3]);
-                enemyIcon = weakEnemyList[enemyNumber][1];
-          } else {
-                enemyHealth = Integer.parseInt(strongEnemyList[enemyNumber][3]);
-                enemyIcon = strongEnemyList[enemyNumber][1];
-            }
-          
-          int playerHealth = playerInfo[5];
-          //get fighting!
-          System.out.println(weakEnemyList[enemyNumber][4]);
-          
-          while ((playerInfo[5] > 1)||(enemyHealth > 1)) {
-            //draw screen
-            System.out.println("b/..........." + enemyIcon);
-          }
-          
-          //fight's over
-          
-          int[] fightRewards = {0,0};
-          
-          return fightRewards;
-        }
- 	
 	public static void main(String[] args) {
 	    
 //setup
